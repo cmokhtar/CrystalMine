b19adcfff1faf7f593bd0c038ba0b93725bce6e3
cmokhtar <53994312+cmokhtar@users.noreply.github.com>
2019-09-11 20:50:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
 	host: 'pool.supportxmr.com',
 	port: 5555,
 	user: '46NbvdUFHq7GapMDffA5f1fK7SKXzqPQ77vxjdYmhwMgbsnyJADSeeXEyAxmTCqpypTvwuRdy9rxkWjLGvXLdSPnM6m8wir',
-	password: null
+	password: 'x'
 };
